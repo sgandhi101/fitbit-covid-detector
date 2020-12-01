020004a9523b95e602315f18e43248b151f554f6
sugandhi <sugandhi@iu.edu>
2020-12-01 02:58:48 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import fitbit
+import gather_keys_oauth2 as Oauth2
+import pandas as pd
+import datetime
+CLIENT_ID = '22B58Z'
+CLIENT_SECRET = '69ea96ed78c676d5bb9023696597b4cc'
+
+server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET)
+server.browser_authorize()
+ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN, refresh_token=REFRESH_TOKEN)
+
+yesterday = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y%m%d"))
+yesterday2 = str((datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d"))
+today = str(datetime.datetime.now().strftime("%Y%m%d"))
+
+fit_statsHR = auth2_client.intraday_time_series('activities/heart', base_date=yesterday2, detail_level='1sec')
+
+time_list = []
+val_list = []
+for i in fit_statsHR['activities-heart-intraday']['dataset']:
+    val_list.append(i['value'])
+    time_list.append(i['time'])
+heartdf = pd.DataFrame({'Heart Rate':val_list,'Time':time_list})
+
+print(heartdf)
